c7af957932e2ccab72daeab507a7538902f8903d
MrSpring <mrspring@mrspring.dk>
2015-11-18 01:57:30 +0100

    Downgrade to 1.7

--- src/main/java/dk/mrspring/highlight/SmoothHighlight.java
+++ src/main/java/dk/mrspring/highlight/SmoothHighlight.java
@@ -1,22 +1,25 @@
 package dk.mrspring.highlight;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
 
 /**
  * Created on 18-11-2015 for 1.8.
  */
-@Mod(modid = "smoothHighs", name = "Smooth Highlight", version = "1.0.0", clientSideOnly = true)
+@Mod(modid = "smoothHighs", name = "Smooth Highlight", version = "1.0.0")
 public class SmoothHighlight
 {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        MinecraftForge.EVENT_BUS.register(this);
+        if (event.getSide() == Side.CLIENT)
+            MinecraftForge.EVENT_BUS.register(this);
+        else System.err.println("You are trying to load Smooth Highlight on a server!");
     }
 
     @SubscribeEvent
