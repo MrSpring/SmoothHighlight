900e49ca20425a1ee31393bd129157388b481982
MrSpring <mrspring@mrspring.dk>
2015-11-22 17:25:10 +0100

    Fix for mods overriding default highlight

--- src/main/java/dk/mrspring/highlight/SmoothHighlight.java
+++ src/main/java/dk/mrspring/highlight/SmoothHighlight.java
@@ -2,6 +2,7 @@ package dk.mrspring.highlight;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
@@ -22,7 +23,7 @@ public class SmoothHighlight
         else System.err.println("You are trying to load Smooth Highlight on a server!");
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void highlight(DrawBlockHighlightEvent event)
     {
         GL11.glEnable(GL11.GL_LINE_SMOOTH);
